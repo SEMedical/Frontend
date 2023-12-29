403165a3013dae24c7c73c1d38a22007637206e4
DaenerysTar <861066998@qq.com>
2023-12-29 23:22:26 +0800

    前端页面合并

--- api/sport.js
+++ api/sport.js
@@ -1,5 +1,25 @@
+
 import request from '@/utils/request'
 
 export default{
-
+	realTimeHeartRate() {
+		return request(`/api/sports/realTimeHeartRate`, 'GET')
+			.then(response => {
+			console.log('后端响应:', response);
+			return response.result.heart_rate;
+		})
+		.catch(error => {
+			throw error;
+		});
+	},
+	getSportRecord() {
+		return request(`/api/sports/getSportRecord`, 'GET')
+			.then(response => {
+			console.log('后端响应:', response);
+			return response.result.array;
+		})
+		.catch(error => {
+			throw error;
+		});
+	}
 }
