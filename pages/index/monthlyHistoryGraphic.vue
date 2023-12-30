d448761b1c6172e10a01403a7634d56fa3f1e252
Karry-Angel <2271576069@qq.com>
2023-12-30 17:48:45 +0800

    完成selectMonth页面，monthlyHistoryGraphic页面可以从selectMonth页面成功获取日期参数

--- pages/index/monthlyHistoryGraphic.vue
+++ pages/index/monthlyHistoryGraphic.vue
@@ -1,8 +1,35 @@
 <template>
+	
 </template>
 
 <script>
+import { ref, onMounted} from 'vue';
+import monthlyBloodSugarData from '@/api/monthlyOrWeeklyHistory.js';
+
+export default{
+	data(){
+		return{
+			highStatistic: ref([]),    //存储高血糖概率值
+			normalStatistic: ref([]),   //存储正常血糖概率值
+			lowStatistic: ref([]),    //存储低血糖概率值
+			//存储当天的血糖数据
+			dayBloodSugar:[],
+			loadedDate : {
+				year : 2023,
+				month : 12,
+				day : 1,
+			},  //要加载的日期数据
+		}
+	},
+	
+	//获取从selectMonth页面传递过来的参数
+	onLoad: function (option) {
+		this.loadedDate = JSON.parse(option.selectedDate);
+		console.log(this.loadedDate);
+	},
+}
 </script>
 
 <style>
+	
 </style>
--- pages/index/selectMonth.vue
+++ pages/index/selectMonth.vue
@@ -42,12 +42,14 @@ export default {
 	                year,
 	                months,
 	                month,
+					days:1,
 	                value: [9999, month - 1],
 	                visible: true,
 	                indicatorStyle: `height: 50px;`,
 					selectedDate: {
 						year: this.year,
 						month: this.month,
+						day:this.days,
 				    },
 	            }
 	        },
@@ -57,15 +59,17 @@ export default {
 	        const val = e.detail.value
 	        this.year = this.years[val[0]]
 	        this.month = this.months[val[1]]
+			this.days=1;
 			
 			this.selectedDate = {
 			    year: this.year,
 			    month: this.month,
+				day:this.days,
 			};
 	    },
 		leadToRecords(){
 			const currentDate = new Date(); // 当前日期
-			const selectedDate = new Date(this.year, this.month - 1); // 选择的日期
+			const selectedDate = new Date(this.year, this.month - 1, 1); // 选择的日期
 			
 			// 检查选择的日期是否在当前日期或之前
 			if (selectedDate <= currentDate) {
